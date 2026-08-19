3db0820f42d3213ffd369dee6a2c875bb15ae023
boynat6060 <boynat6060@ccsdschools.com>
2026-08-19 10:39:49 -0400

    js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-//testing2
+document.addEventListener('keydown', function(event) {
+    if (event.key === '.') {
+        changeSize();
+    }
+});
+
+var sz = 1;
+
+function changeSize() {
+    document.getElementById("pl1").style.lineHeight = sz;
+    console.log("Key pressed.");
+    sz++;
+    console.log(sz);
+}
